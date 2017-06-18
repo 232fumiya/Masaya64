767f293148b0be1c7655ff32a8b38acc4eb574f8
232fumiya <21311232fn@tama.ac.jp>
2017-06-18 15:15:25 +0900

    ステートパターン

---
+++ Assets/Scripts/Stages/StagesList.cs
@@ -0,0 +1,19 @@
+﻿namespace Masaya64.Controller{
+	public static class StagesList{
+		public enum stages 
+		{
+			stage_select 	= 0,
+			stage			= 1,
+			result			= 2
+		}
+		private static string[] stage_name = new string[] 
+		{
+			"StageSelect",
+			"Stage",
+			"Result"
+		};
+		public static string getStageName(stages stage){
+			return stage_name [(int)stage];
+		}
+	}
+}
